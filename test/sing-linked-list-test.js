7aee0ae3cce22fc50ef499b3754a82e3faafa06b
Dan Schwartz <dpschwartz@gmail.com>
2016-09-02 17:30:40 -0700

    add SLL & test filess. instantiate SLL. insertHead and second ele to SLL

---
+++ test/sing-linked-list-test.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const expect = require('chai').expect
+const SLL = require('../lib/sing-linked-list')
+
+describe('instantiate SLL', () => {
+  it('should create an empty list', () => {
+    const sll = new SLL()
+    expect(sll).to.be.an.instanceof(SLL)
+    expect(sll).to.deep.equal({head: null, tail: null})
+  })
+})
