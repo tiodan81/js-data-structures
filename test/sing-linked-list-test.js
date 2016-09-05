a882963a22405224890cd39a1df1df2ed4620f9e
Dan Schwartz <dpschwartz@gmail.com>
2016-09-04 21:15:16 -0700

    refactor SLL.insertHead to take value instead of node

--- test/sing-linked-list-test.js
+++ test/sing-linked-list-test.js
@@ -7,7 +7,6 @@ describe('instantiate SLL', () => {
   it('should create an empty list', () => {
     const sll = new SLL()
     expect(sll).to.be.an.instanceof(SLL)
-    expect(sll).to.deep.equal({head: null, tail: null})
   })
 })
 
