58c407998806b0ecd32accc75885aa44b1247af4
Dan Schwartz <dpschwartz@gmail.com>
2016-10-01 15:01:21 -0700

    finish SLL, graph.setVertexValue

--- test/sing-linked-list-test.js
+++ test/sing-linked-list-test.js
@@ -3,31 +3,64 @@
 const expect = require('chai').expect
 const SLL = require('../lib/sing-linked-list')
 
-describe('test singly linked list', () => {
+describe.only('test singly linked list', () => {
   describe('instantiate SLL', () => {
     it('should create an empty list', () => {
       const sll = new SLL()
       expect(sll).to.be.an.instanceof(SLL)
+      expect(sll.length).to.equal(0)
     })
   })
 
-  describe('test insertHead', () => {
+  describe('test insertHead()', () => {
+    it('should fail if no value given', () => {
+      const sll = new SLL()
+      const badInsert = () => sll.insertHead()
+      expect(badInsert).to.throw(ReferenceError)
+    })
     it('should add a new node to head', () => {
       const sll = new SLL()
       sll.insertHead(1)
-      expect(sll.head.data).to.equal(1)
-      expect(sll.head.next).to.be.null
-      expect(sll.head.prev).to.be.null
+      sll.insertHead(2)
+      expect(sll.head.data).to.equal(2)
+      expect(sll.head.next).to.eql({ data: 1, next: null })
       expect(sll.current).to.be.null
-      expect(sll.length).to.equal(1)
-    })
-    it('should fail if no value given', () => {
-      const sll = new SLL()
-      expect(sll.insertHead()).to.be.an.instanceof(ReferenceError)
+      expect(sll.length).to.equal(2)
     })
   })
 
-  describe('test search', () => {
+  describe('test insertAfter()', () => {
+    it('should fail if no old value given or old value not found')
+    it('should fail if no new value given')
+    it('should insert newNode at head if list empty')
+    it('should insert newNode after oldNode if list !empty')
+    it('should increment SLL.length')
+  })
+
+  describe('test traverse()', () => {
+    let sll
+
+    beforeEach(() => {
+      sll = new SLL()
+      sll.insertHead(1)
+      sll.insertHead(2)
+      sll.insertHead(3)
+    })
+
+    it('should fail if called on an empty list', () => {
+      const empty = new SLL()
+      const emptyTraverse = () => empty.traverse()
+      expect(emptyTraverse).to.throw(RangeError)
+    })
+
+    it('should return the last node', () => {
+      const expected = { data: 1, next: null }
+      const last = sll.traverse()
+      expect(last).to.eql(expected)
+    })
+  })
+
+  describe('test search()', () => {
     let sll
 
     beforeEach(() => {
@@ -35,13 +68,10 @@ describe('test singly linked list', () => {
       sll.insertHead(1)
       sll.insertHead(2)
     })
+    it('should return false if no match found')
+
     it('should return this.current if value matches', () => {
       // const
-    })
-    it('should set current and return matching value', () => {
-      console.log(sll);
-      expect(sll.search(1)).to.equal(1)
-      expect(sll.current).to.equal({data: 1, next: null, prev: null})
     })
   })
 })
